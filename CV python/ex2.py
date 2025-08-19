202b492c5f895c994ad8201e5c9e89f4c4c5ec8c
Athityan A <148308105+athi224456@users.noreply.github.com>
2025-08-19 11:52:39 +0530

    Update.py

--- CV python/ex2.py
+++ CV python/ex2.py
@@ -1,9 +1,1 @@
-import cv2
-import matplotlib.pyplot as plt
-
-img = cv2.imread('vijay.jpeg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
-plt.axis('off')
-plt.show()
 
