912b06429d58b0f517c1c1d636e5e1cda63d91f7
Athityan A <148308105+athi224456@users.noreply.github.com>
2025-08-20 11:31:31 +0530

    Delete CV python directory

--- CV python/gaussian.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt         
-img = cv2.imread("vijay.jpeg")
-gaussian = cv2.GaussianBlur(img,(5,15),5)
-plt.imshow(cv2.cvtColor(gaussian,cv2.COLOR_BGR2RGB))
-plt.axis("off")
-plt.show()
